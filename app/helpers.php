c50aff0e1b8a6ec78b5495714eb61f073c8785a3
Agostinho Ramos <agostinhopina095@gmail.com>
2020-11-08 04:35:14 +0000

    Test mail function

--- app/helpers.php
+++ app/helpers.php
@@ -14,37 +14,37 @@ function return_path($path){
 
 function send_email($object){
 
-    Mail::to($object["email"])->send(new ClientMail([
-        "name" => $object["name"],
-        "message" => $object["message"],
-        "email" => $object["email"]
-    ]));
+    // Mail::to($object["email"])->send(new ClientMail([
+    //     "name" => $object["name"],
+    //     "message" => $object["message"],
+    //     "email" => $object["email"]
+    // ]));
 
-    return True;
+    // return True;
 
-    // $to = 'agostinhopina095@gmail.com';
-    // $subject = 'Marriage Proposal';
-    // $from = 'general@agostinhopinaramos.com';
+    $to = 'agostinhopina095@gmail.com';
+    $subject = 'Marriage Proposal';
+    $from = 'general@agostinhopinaramos.com';
 
-    // // To send HTML mail, the Content-type header must be set
-    // $headers  = 'MIME-Version: 1.0' . "\r\n";
-    // $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    // To send HTML mail, the Content-type header must be set
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-    // // Create email headers
-    // $headers .= 'From: ' . $from . "\r\n" .
-    // 'Reply-To: ' . $from . "\r\n" .
-    // 'X-Mailer: PHP/' . phpversion();
+    // Create email headers
+    $headers .= 'From: ' . $from . "\r\n" .
+    'Reply-To: ' . $from . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-    // // Compose a simple HTML email message
-    // $message = '<html><body>';
-    // $message .= '<h1 style="color:#f40;">Hi Jane!</h1>';
-    // $message .= '<p style="color:#080;font-size:18px;">Will you marry me?</p>';
-    // $message .= '</body></html>';
+    // Compose a simple HTML email message
+    $message = '<html><body>';
+    $message .= '<h1 style="color:#f40;">Hi Jane!</h1>';
+    $message .= '<p style="color:#080;font-size:18px;">Will you marry me?</p>';
+    $message .= '</body></html>';
 
-    // // Sending email
-    // if (mail($to, $subject, $message, $headers)) {
-    //     return True;
-    // } 
+    // Sending email
+    if (mail($to, $subject, $message, $headers)) {
+        return True;
+    }
     
     return False;
 }
