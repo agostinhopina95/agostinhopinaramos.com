fdfeb25542cd7a030f20a30bf0c49d010ad3a7d1
Agostinho Ramos <agostinhopina095@gmail.com>
2020-11-08 05:23:35 +0000

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -14,30 +14,11 @@ function return_path($path){
 
 function send_email($object){
 
-    $to = 'agostinhopina095@gmail.com';
-    $subject = 'APR Form Message';
-    $from = 'general@agostinhopinaramos.com';
-
-    // To send HTML mail, the Content-type header must be set
-    $headers  = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    // Create email headers
-    $headers .= 'From: ' . $from . "\r\n" .
-    'Reply-To: ' . $from . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-    // Compose a simple HTML email message
-    $message = new ClientMail([
+    Mail::to($object["email"])->send(new ClientMail([
         "name" => $object["name"],
         "message" => $object["message"],
         "email" => $object["email"]
-    ]);
-
-    // Sending email
-    if (mail($to, $subject, $message, $headers)) {
-        return True;
-    }
+    ]));
     
-    return False;
+    return True;
 }
